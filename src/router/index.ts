1564a5ee6a719190b52de10e0b4ad9dd8e9203ce
Jord <jord018@users.noreply.github.com>
2026-07-18 10:21:17 +0700

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '@/views/EventListView.vue'
+import AboutView from '@/views/AboutView.vue'
+import StudentView from '@/views/StudentInfoView.vue'
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list-view',
+      component: EventListView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: StudentView,
+    },
+  ],
+})
+
+export default router
