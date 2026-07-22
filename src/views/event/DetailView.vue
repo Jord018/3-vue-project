3694394214f6649317faebb6807d041d5e1728c1
Jord <jord018@users.noreply.github.com>
2026-07-22 20:59:16 +0700

    4 1.6

--- src/views/event/DetailView.vue
+++ src/views/event/DetailView.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
 const props = defineProps<{
     event: Event
 } > ()
 const { event } = toRefs(props)
+const store = useMessageStore()
+const { message } = storeToRefs(store)
 </script>
 <template>
+ <div id="flashMessage" v-if="message">
+   <h4>{{ message }}</h4>
+ </div>
  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
  <p>{{ event.description }}</p>
 </template>
